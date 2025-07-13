bf257424081ba4a2e878fd298036ca04be2179cb
psubb <pranavsubbiah@gmail.com>
2025-07-12 23:07:36 -0700

    IMplement findUserByEmail

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -1,3 +1,27 @@
 import pool from '../db/db';
 
-export async function createUser(email: string, password: string)
+export async function createUser(email: string, passwordHash: string) {
+    const query = `
+        INSERT INTO users (email, password_hash)
+        VALUES ($1, $2)
+        RETURNING *;
+    `;
+
+    const values = [email, passwordHash];
+
+    const result = await pool.query(query, values);
+    return result.rows[0];
+}
+
+export async function findUserByEmail(email: string) {
+    const query = `
+        SELECT * FROM users
+        WHERE email = $1
+        LIMIT 1;
+    `;
+    
+    const values = [email];
+    const result = await pool.query(query, values);
+
+    return result.rows[0];
+}
