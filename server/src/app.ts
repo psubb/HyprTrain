885918e8cfaa0c39ad2fe5c7ceac61f03a194483
psubb <pranavsubbiah@gmail.com>
2025-07-21 21:26:27 -0700

    Wired up programs routes in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response, Application } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from "./routes/authRoutes"
+import authRoutes from "./routes/authRoutes";
+import programRoutes from "./routes/programRoutes";
 
 dotenv.config();
 const app: Application = express();
@@ -9,6 +10,7 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api', programRoutes);
 
 app.get('/', (_req: Request, res: Response): void => {
     res.send('HyprTrain API is running');
