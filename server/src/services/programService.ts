c3786083d349e2441fc22b508e7c80d2145823a2
psubb <pranavsubbiah@gmail.com>
2025-07-21 20:57:06 -0700

    Implement createProgram and generateDefaultProgramName in programService.ts

---
+++ server/src/services/programService.ts
@@ -0,0 +1,20 @@
+import { insertProgram, countProgramsForUser } from "models/programModel";
+
+export async function createProgram(userId: string, data: { name?: string; duration_weeks: number}) {
+    const { name, duration_weeks } = data;
+    const cleanName = name?.trim();
+    let finalName: string;
+
+    if (!cleanName){
+        finalName = await generateDefaultProgramName(userId);
+    } else {
+        finalName = cleanName!;
+    }
+
+    await insertProgram(userId, finalName, duration_weeks);
+}
+
+export async function generateDefaultProgramName(userId: string): Promise<string> {
+    const programNumber = await countProgramsForUser(userId);
+    return `My Program ${programNumber + 1}`;
+}
