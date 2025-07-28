eeb505ddeb66b7a4eda13cca62b075bcf96b6d4d
psubb <pranavsubbiah@gmail.com>
2025-07-27 20:46:28 -0700

    Add findExerciseByNameForUser() in exerciseModel.ts

--- server/src/models/exerciseModel.ts
+++ server/src/models/exerciseModel.ts
@@ -6,8 +6,22 @@ export async function insertCustomExercise(userId: string, exerciseName: string,
     `INSERT INTO exercises (name, muscle_group_id, user_id, is_default, is_deleted)
     VALUES ($1, $2, $3, $4, $5)
     RETURNING *`,
-    [exerciseName, muscleGroupId, userId, false, false] 
-    );
+    [exerciseName, muscleGroupId, userId, false, false]
+);
+
+    if (result.rows.length === 0){
+        throw new Error("Failed to insert exercise");
+    }
 
     return result.rows[0];
+}
+
+export async function findExerciseByNameForUser(userId: string, exerciseName: string): Promise<Exercise | null> {
+    const result = await pool.query(
+    `SELECT * FROM exercises
+    WHERE exercise_name ILIKE $1 AND (user_id = $2 OR user_id IS NULL)`,
+    [exerciseName, userId]
+    );
+
+    return result.rows[0] || null;
 }
