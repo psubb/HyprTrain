de66ea8b5175559d422bc7473b067a9c63f993f5
psubb <pranavsubbiah@gmail.com>
2025-07-28 21:33:15 -0700

    Wire up route

--- server/src/routes/exerciseRoutes.ts
+++ server/src/routes/exerciseRoutes.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
-import { createCustomExercise, deleteCustomExercise } from '../controllers/exerciseController';
+import { createCustomExercise, deleteCustomExercise, getCustomExercises } from '../controllers/exerciseController';
 import { authenticateAccessToken } from '../middleware/authMiddleware';
 
 const router: Router = express.Router();
 
 router.post("/exercises", authenticateAccessToken, createCustomExercise);
 router.delete("/exercises/:id", authenticateAccessToken, deleteCustomExercise);
+router.get("/exercises/custom", authenticateAccessToken, getCustomExercises);
+
 
 export default router;
