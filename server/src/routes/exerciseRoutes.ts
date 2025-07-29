9f005524c30950624454e271b5b7f0dc4ca3a371
psubb <pranavsubbiah@gmail.com>
2025-07-28 20:58:46 -0700

    Wire up routes for deleteCustomExercise

--- server/src/routes/exerciseRoutes.ts
+++ server/src/routes/exerciseRoutes.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express';
-import { createCustomExercise } from '../controllers/exerciseController';
+import { createCustomExercise, deleteCustomExercise } from '../controllers/exerciseController';
 import { authenticateAccessToken } from '../middleware/authMiddleware';
 
 const router: Router = express.Router();
 
-router.post("/exercises", authenticateAccessToken, createCustomExercise)
+router.post("/exercises", authenticateAccessToken, createCustomExercise);
+router.delete("/exercises/:id", authenticateAccessToken, deleteCustomExercise);
 
 export default router;
