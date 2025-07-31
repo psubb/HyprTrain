fe270cba8eb346546608e3d6d84ad2a07a00fdfb
psubb <pranavsubbiah@gmail.com>
2025-07-30 21:43:27 -0700

    Implement getActiveProgram in programController.ts

--- server/src/controllers/programController.ts
+++ server/src/controllers/programController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { createProgram as createProgramService} from '../services/programService';
+import { createProgram as createProgramService, getActiveProgram as getActiveProgramService} from '../services/programService';
 
 export async function createProgram(req: Request, res: Response): Promise<void> {
     const userId = req.user?.userId;
@@ -19,4 +19,20 @@ export async function createProgram(req: Request, res: Response): Promise<void> 
     const program = await createProgramService(userId, { name, duration_weeks });
 
     res.status(201).json({message: 'Program created successfully', program});
+}
+
+export async function getActiveProgram(req: Request, res: Response){
+    const userId = req.user?.userId;
+
+    if (!userId) {
+        res.status(401).json({message: 'Unauthorized: No user ID found.'});
+        return;
+    }
+
+    try {
+        const activeProgram = await getActiveProgramService(userId);
+        res.status(200).json(activeProgram);
+    } catch (err: any){
+        res.status(500).json({message: err.message || "Error when trying to retrieve active program"});
+    }
 }
