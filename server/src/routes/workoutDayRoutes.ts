8d726c43bc2f2211565f6074b3b155c17b8c81b8
psubb <pranavsubbiah@gmail.com>
2025-07-31 20:27:29 -0700

    Wire up route

--- server/src/routes/workoutDayRoutes.ts
+++ server/src/routes/workoutDayRoutes.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express';
-import { addWorkoutDays } from '../controllers/workoutDayController';
+import { addWorkoutDays, getActiveWorkoutDay } from '../controllers/workoutDayController';
 import { authenticateAccessToken } from '../middleware/authMiddleware';
 
 const router: Router = express.Router();
 
 router.post("/programs/:id/workout-days", authenticateAccessToken, addWorkoutDays);
+router.get("/programs/:id/active-day", authenticateAccessToken, getActiveWorkoutDay);
 
 export default router;
