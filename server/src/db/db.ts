ab7503189e73e7407fd8ccfbcd93b2b616f03212
psubb <pranavsubbiah@gmail.com>
2025-08-08 21:31:03 -0700

    Change db pool for deployment

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,11 +1,16 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }, // Needed for Neon
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
 
 export default pool;
